8fa385cc0546a289043f7e101299416b4d7f2c0a
Ishaan488 <ishaanbajpai732004@gmail.com>
2026-03-03 03:56:42 +0530

    fix: countUpUnit, aos fix

--- src/components/CountUpUnit.tsx
+++ src/components/CountUpUnit.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from 'react';
 import CountUp from 'react-countup';
 
 interface CountUpUnitPropsType {
@@ -7,14 +8,23 @@ interface CountUpUnitPropsType {
   label: string;
 }
 
-export default function CountUpUnit({upto, label}: CountUpUnitPropsType) {
+export default function CountUpUnit({ upto, label }: CountUpUnitPropsType) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
     <div className="flex items-center justify-center gap-10 flex-wrap text-white">
       <div className="flex flex-col justify-center items-center">
         <div className="text-4xl md:text-6xl flex items-center justify-center w-32 md:w-48">
           <div className="invisible">+</div>
-          <CountUp end={upto} duration={2} enableScrollSpy />
+          {isMounted ? (
+            <CountUp end={upto} duration={2} enableScrollSpy scrollSpyOnce />
+          ) : (
+            <span>{upto}</span>
+          )}
           <div className="text-red">+</div>
         </div>
         <div className="w-full text-wrap text-center">{label}</div>
